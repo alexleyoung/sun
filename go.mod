0a63f6ee0a038d30188910194ef4964fb44aa4df
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-10-03 13:09:33 -0500

    fetch location from api

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module alexleyoung/weather
 
 go 1.22.1
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}
+	res, err := http.Get(fmt.Sprintf("http://api.openweathermap.org/geo/1.0/zip?zip=%s,%s&appid=%s", os.Getenv("ZIP_CODE"), os.Getenv("COUNTRY_CODE"), os.Getenv("API_KEY")))
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		panic(fmt.Sprintf("status code: %d", res.StatusCode))
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
