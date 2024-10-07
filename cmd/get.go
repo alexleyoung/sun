00fcfa606bbf1482a3ba8f781548ef64de785a82
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-10-07 01:10:13 -0500

    add weather summary at top (low high)

--- cmd/get.go
+++ cmd/get.go
@@ -63,6 +63,12 @@ func getWeather(cmd *cobra.Command, args []string) {
 		return
 	}
 
+	// Print current weather
+	fmt.Println("Current Weather:")
+	fmt.Printf("Low: %.1f°F, High: %.1f°F\n\n", weather.Forecast.Forecastday[0].Day.MinTempF, weather.Forecast.Forecastday[0].Day.MaxTempF)
+
+	// Print today's forecast
+	fmt.Println("Todays Forecast:")
 	for hour := range weather.Forecast.Forecastday[0].Hour {
 		hourInfo := weather.Forecast.Forecastday[0].Hour[hour]
 		location, err := time.LoadLocation(weather.Location.TzID)
@@ -71,6 +77,7 @@ func getWeather(cmd *cobra.Command, args []string) {
 			return
 		}
 
+		// Get the current hour
 		currentHour, err := strconv.ParseInt((time.Now().In(location).Format("15:04")[0:2]), 10, 0)
 
 		if err != nil {
@@ -78,10 +85,12 @@ func getWeather(cmd *cobra.Command, args []string) {
 			return
 		}
 
+		// If the current hour is greater than the hour we are iterating through, skip it
 		if currentHour > int64(hour) {
 			continue
 		}
 
+		// Format the time
 		hourTime := strings.Split(hourInfo.Time, " ")[1]
 		hourTimeObj, err := time.Parse("15:04", hourTime)
 		if err != nil {
@@ -89,7 +98,7 @@ func getWeather(cmd *cobra.Command, args []string) {
 			return
 		}
 		
-		fmt.Println("Todays Forecast:")
+		// Format the forecast message
 		message := fmt.Sprintf("%s: %.1f°F", hourTimeObj.Format("3:04 PM"), hourInfo.TempF)
 		if hourInfo.WillItRain == 1 {
 			message += " It will rain!\n"
