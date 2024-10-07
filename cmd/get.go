d98b00d2f9fdd821a1581d25e86facf8b2eaf04f
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-10-07 00:19:53 -0500

    sun get req

--- cmd/get.go
+++ cmd/get.go
@@ -1,6 +1,16 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"alexleyoung/sun/types"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 var getCmd = &cobra.Command{
 	Use:   "get",
@@ -10,11 +20,50 @@ var getCmd = &cobra.Command{
 }
 
 func getWeather(cmd *cobra.Command, args []string) {
+	// if no api key set, prompt user for api key
+	if viper.Get("apiKey") == "" {
+		fmt.Print("Enter API key: ")
+		var apiKey string
+		fmt.Scanln(&apiKey)
+		viper.Set("apiKey", apiKey)
+	}
+
+	// if no location set, prompt user for location
+	if viper.Get("location") == "" {
+		fmt.Print("Enter Location (zip OR city OR state OR country): ")
+		var location string
+		fmt.Scanln(&location)
+		viper.Set("location", location)
+	} 
+
+	location := viper.Get("location")
+	apiKey := viper.Get("apiKey")
+
+	res, err := http.Get(fmt.Sprintf("http://api.weatherapi.com/v1/forecast.json?key=%s&q=%s", apiKey, location))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer res.Body.Close()
+	fmt.Println("Status:", res.Status)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	var weather types.Weather
+
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println(weather.Current.TempC)
 }
 
-var location string
-
 func init() {
-	getCmd.Flags().StringVarP(&location, "location", "l", "", "Location to get weather information for")
 	rootCmd.AddCommand(getCmd)
 }
